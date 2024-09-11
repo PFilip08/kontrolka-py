b7f7dac449181773a2be7538a3d6900c82518bbe
PFilip <2008filip@wp.pl>
2024-09-11 20:24:22 +0200

    v1.0.3 - sekjurity

--- boot.py
+++ boot.py
@@ -3,15 +3,16 @@ import time
 import uota
 import machine
 import secrets
+import network
 
 ssid = secrets.ssid
 passwd = secrets.passwd
 do_ota_update = secrets.do_ota_update
 
+wlan = network.WLAN(network.STA_IF)
+
 def do_connect():
-    import network
     network.country("PL")
-    wlan = network.WLAN(network.STA_IF)
     wlan.active(False)
     wlan.active(True)
     # wlan.config(txpower=5)
@@ -22,6 +23,9 @@ def do_connect():
             l1 = machine.Pin(1, machine.Pin.OUT)
             l2 = machine.Pin(2, machine.Pin.OUT)
             l3 = machine.Pin(4, machine.Pin.OUT)
+            l1.off()
+            l2.off()
+            l3.off()
             l1.on()
             time.sleep(1)
             l2.on()
@@ -35,18 +39,30 @@ def do_connect():
             pass
     print('network config:', wlan.ifconfig())
 
+def check_wifi_connection():
+    if not wlan.isconnected():
+        print('Wi-Fi disconnected, trying to reconnect...')
+        do_connect()
+
 do_connect()
 
-if do_ota_update and uota.check_for_updates():
-    led=machine.Pin(1, machine.Pin.OUT)
-    led.on()
-    time.sleep(0.5)
-    led.off()
-    time.sleep(0.5)
-    led.on()
-    time.sleep(0.5)
-    led.off()
-    print('Updating uota...')
-    uota.install_new_firmware()
-    machine.reset()
-else: print('no update');
+try:
+    if do_ota_update and uota.check_for_updates():
+        led = machine.Pin(1, machine.Pin.OUT)
+        led.on()
+        time.sleep(0.5)
+        led.off()
+        time.sleep(0.5)
+        led.on()
+        time.sleep(0.5)
+        led.off()
+        print('Updating uota...')
+        uota.install_new_firmware()
+        machine.reset()
+    else: print('no update');
+except Exception as e:
+        print("Błąd przy łączeniu z serwerem OTA:", e)
+
+
+import webrepl
+webrepl.start()
--- main.py
+++ main.py
@@ -1,50 +1,49 @@
-# main.py
+import time
 import utime
 from machine import Pin
 import urequests
 import secrets
+from boot import check_wifi_connection
 
 api_url = secrets.api_url
+api_ok = True
 
 leds = {
-    # duże, do funkcji
-    "led1": Pin(1, Pin.OUT), # GPIO 4
-    "led2": Pin(2, Pin.OUT), # GPIO 2
-    "led3": Pin(4, Pin.OUT), # GPIO 1
-
-    # małe, do statusu pc
-    "led_s1": Pin(6, Pin.OUT), # GPIO 6
-    "led_s2": Pin(8, Pin.OUT), # GPIO 8
-    "led_s3": Pin(10, Pin.OUT) # GPIO 10
+    "led1": Pin(1, Pin.OUT),
+    "led2": Pin(2, Pin.OUT),
+    "led3": Pin(4, Pin.OUT),
+    "led_s1": Pin(6, Pin.OUT),
+    "led_s2": Pin(8, Pin.OUT),
+    "led_s3": Pin(10, Pin.OUT)
 }
 
 buttons = {
-    # duże, do funkcji
-    "button1": Pin(7, Pin.IN, Pin.PULL_UP), # GPIO 7
-    "button2": Pin(5, Pin.IN, Pin.PULL_UP), # GPIO 5
-    "button3": Pin(3, Pin.IN, Pin.PULL_UP), # GPIO 3
-
-    # małe, do pc
-    "sw1": Pin(12, Pin.IN, Pin.PULL_UP), # GPIO 12
-    "sw1b": Pin(13, Pin.IN, Pin.PULL_UP), # GPIO 13
-    "sw2": Pin(11, Pin.IN, Pin.PULL_UP), # GPIO 11
-    "sw2b": Pin(14, Pin.IN, Pin.PULL_UP), # GPIO 14
-    "sw3": Pin(9, Pin.IN, Pin.PULL_UP), # GPIO 9
-    "sw3b": Pin(15, Pin.IN, Pin.PULL_UP) # GPIO 15
+    "button1": Pin(7, Pin.IN, Pin.PULL_UP),
+    "button2": Pin(5, Pin.IN, Pin.PULL_UP),
+    "button3": Pin(3, Pin.IN, Pin.PULL_UP),
+    "sw1": Pin(12, Pin.IN, Pin.PULL_UP),
+    "sw1b": Pin(13, Pin.IN, Pin.PULL_UP),
+    "sw2": Pin(11, Pin.IN, Pin.PULL_UP),
+    "sw2b": Pin(14, Pin.IN, Pin.PULL_UP),
+    "sw3": Pin(9, Pin.IN, Pin.PULL_UP),
+    "sw3b": Pin(15, Pin.IN, Pin.PULL_UP)
 }
+
 
 def button_callback(pin, button_name):
     if pin.value() == 0:
         try:
-            response = urequests.post(api_url+'/button', json={"button": button_name})
+            response = urequests.post(api_url + '/button', json={"button": button_name})
             print(f"Wysłano żądanie do API z {button_name}: {response.status_code}")
             response.close()
         except Exception as e:
             print(f"Błąd przy wysyłaniu żądania dla {button_name}: {e}")
 
+
 def check_api_status():
+    global api_ok
     try:
-        response = urequests.get(api_url+'/led')
+        response = urequests.get(api_url + '/led')
         if response.status_code == 200:
             data = response.json()
             for led_name, led_pin in leds.items():
@@ -52,15 +51,37 @@ def check_api_status():
                     led_pin.on()
                 else:
                     led_pin.off()
+            api_ok = True
         response.close()
     except Exception as e:
         print("Błąd przy odczytywaniu statusu z API:", e)
+        api_ok = False
+
+
+def api_unreachable():
+    global api_ok
+    l3 = leds["led3"]
+
+    while not api_ok:
+        print("API nieosiągalne, próba ponownego połączenia...")
+        l3.on()
+        time.sleep(1)
+        l3.off()
+        time.sleep(1)
+        check_api_status()  # Ciągle sprawdzaj, czy API wróciło
+
+    print("Połączenie z API przywrócone")
 
 
 for button_name, button_pin in buttons.items():
     button_pin.irq(trigger=Pin.IRQ_FALLING, handler=lambda pin, name=button_name: button_callback(pin, name))
 
-
 while True:
-    check_api_status()
+    check_wifi_connection()
+
+    if api_ok:
+        check_api_status()
+    else:
+        api_unreachable()
+
     utime.sleep(2)

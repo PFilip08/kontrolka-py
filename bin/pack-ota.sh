9afe65f3a81c3f6af874039d060ee401b66c0206
PFilip <2008filip@wp.pl>
2025-01-11 23:48:57 +0100

    update pack-ota.sh script

--- bin/pack-ota.sh
+++ bin/pack-ota.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# By PFilip - ©2024
+# By PFilip - ©2024-2025
 VERSION=$(<../version)
 
+echo "Version: $VERSION"
+echo "Creating OTA package..."
 tar --exclude-from=exclude-file -zcvf out/kontrolka-py-"$VERSION".tgz ../
+echo "OTA package created: out/kontrolka-py-$VERSION.tgz"
+echo -e "$VERSION;kontrolka-py-$VERSION.tgz\n// $(date)" > out/latest
+echo "Latest version file updated: out/latest"
+echo "Done."
